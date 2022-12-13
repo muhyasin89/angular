574f1a78b04c2b49c9d5f716dc846e36872fc4ee
muhyasin89 <muhyasin89@gmail.com>
2022-12-13 09:43:01 +0700

    [feature/login]make calendar

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ParkerRandalWeb;
 using MudBlazor.Services;
+
 using Blazored.LocalStorage;
 using ParkerRandalWeb.Authorization;
 using Microsoft.AspNetCore.Components.Authorization;
+using Radzen;
 
 namespace ParkerRandalWeb
 {
@@ -18,7 +20,14 @@ namespace ParkerRandalWeb
             builder.Services.AddMudServices();
             builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddAuthorizationCore();
+
+            builder.Services.AddScoped<DialogService>();
+            builder.Services.AddScoped<NotificationService>();
+            builder.Services.AddScoped<TooltipService>();
+            builder.Services.AddScoped<ContextMenuService>();
+
             builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+       
 
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
---
+++ Models/Tasks.cs
@@ -0,0 +1,13 @@
+﻿using MudBlazor;
+
+namespace ParkerRandalWeb.Models
+{
+    public class Appointment
+    {
+        public int id;
+        public DateTime Start;
+        public DateTime End;
+
+        public string Text = String.Empty;
+    }
+}
